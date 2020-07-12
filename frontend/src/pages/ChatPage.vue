7edc0623efb73a607ff8b77ef8fead65d8aa8a13
nari-z <go.to18ishi@gmail.com>
2020-07-12 16:42:51 +0900

    add chat page

---
+++ frontend/src/pages/ChatPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        Chat Page
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'ChatPage',
+
+    data: () => ({}),
+  }
+</script>
