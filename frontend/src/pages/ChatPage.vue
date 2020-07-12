68d3570a3aebb517e319dc6081989273647148a7
nari-z <go.to18ishi@gmail.com>
2020-07-12 23:23:52 +0900

    sample

--- frontend/src/pages/ChatPage.vue
+++ frontend/src/pages/ChatPage.vue
@@ -1,9 +1,36 @@
 <template>
   <v-container>
-    <v-row class="text-center">
+    <h1>Chat Page, Room Title</h1>
+    <v-row>
       <v-col cols="12">
-        Chat Page
+        <!-- TODO: change to list -->
+        <v-card
+          outlined
+        >
+          <v-list-item three-line style="text-align: left;">
+            <v-list-item-content>
+              <v-list-item-title >1. No name</v-list-item-title>
+              <v-list-item-subtitle>this message</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+        <v-card
+          outlined
+        >
+          <v-list-item three-line style="text-align: left;">
+            <v-list-item-content>
+              <v-list-item-title >2. No name</v-list-item-title>
+              <v-list-item-subtitle>ahaha</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
       </v-col>
+    </v-row>
+    <v-row>
+      <v-text-field label="Message" />
+      <v-btn>
+        <v-icon>mdi-pencil</v-icon>
+      </v-btn>
     </v-row>
   </v-container>
 </template>
