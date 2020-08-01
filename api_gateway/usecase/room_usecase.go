3e8970b2828de8ac274557b0cca55f9e46650255
enokawa <enokawa00@gmail.com>
2020-08-01 17:23:51 +0900

    Add DB Connection method

--- api_gateway/usecase/room_usecase.go
+++ api_gateway/usecase/room_usecase.go
@@ -11,6 +11,7 @@ import (
 type RoomUsecase interface {
 	CreateUUID() google_uuid.UUID
 	InsertDB(raw domain.RoomEntity) error
+	DBConn() (db *sql.DB)
 }
 
 type roomUsecase struct{}
@@ -23,18 +24,28 @@ func (r roomUsecase) CreateUUID() google_uuid.UUID {
 	return google_uuid.UUID(google_uuid.New())
 }
 
-func (r roomUsecase) InsertDB(raw domain.RoomEntity) error {
-	db, err := sql.Open("mysql", "root:mypassword@tcp(0.0.0.0:3306)/tmpchat")
+func (r roomUsecase) DBConn() (db *sql.DB) {
+	driver := "mysql"
+	user := "root"
+	password := "mypassword"
+	host := "0.0.0.0"
+	dbname := "tmpchat"
+	db, err := sql.Open(driver, user+":"+password+"@"+"("+host+")"+"/"+dbname)
+	//	"root:mypassword@tcp(0.0.0.0:3306)/tmpchat")
 	if err != nil {
-		return err
+		panic(err.Error())
 	}
-	defer db.Close()
+	return db
+}
 
+func (r roomUsecase) InsertDB(raw domain.RoomEntity) error {
+	db := r.DBConn()
 	ins, err := db.Query("insert into room values (default, ?, ?, default, default, ?)", raw.UUID, raw.Title, nil)
 	if err != nil {
 		return err
 	}
 	defer ins.Close()
+	defer db.Close()
 
 	return err
 }
