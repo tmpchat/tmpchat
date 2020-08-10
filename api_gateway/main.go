756ba8bd1d49fd645bc1356acfca45351d32c6e6
nari-z <go.to18ishi@gmail.com>
2020-08-10 15:51:49 +0900

    use gollira/mux

--- api_gateway/main.go
+++ api_gateway/main.go
@@ -6,23 +6,26 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
+
 	"github.com/tmpchat/tmpchat/api_gateway/controller"
 )
 
 func main() {
 	// routing
+	router := mux.NewRouter()
 	con := controller.NewRoomController()
-	http.HandleFunc("/room/create", con.Create)
-	http.HandleFunc("/room/find", con.Find)
-	http.HandleFunc("/room/list", con.List)
-	http.HandleFunc("/room/update-title", con.UpdateTitle)
+	router.HandleFunc("/room/create", con.Create)
+	router.HandleFunc("/room/find", con.Find)
+	router.HandleFunc("/room/list", con.List)
+	router.HandleFunc("/room/update-title", con.UpdateTitle)
 
 	// listen
 	apiGatewayHost := os.Getenv("API_GATEWAY_HOST")
 	if apiGatewayHost == "" {
 		apiGatewayHost = "127.0.0.1:8888"
 	}
-	log.Fatal(http.ListenAndServe(apiGatewayHost, nil))
+	log.Fatal(http.ListenAndServe(apiGatewayHost, router))
 
 	fmt.Println("Done")
 }
