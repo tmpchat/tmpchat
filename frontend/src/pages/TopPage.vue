128a43474ba4f65e4305e06bdb7d4f9d7125b601
nari-z <go.to18ishi@gmail.com>
2020-08-23 17:56:21 +0900

    add room title list

--- frontend/src/pages/TopPage.vue
+++ frontend/src/pages/TopPage.vue
@@ -1,24 +1,30 @@
 <template>
   <v-container>
     <h1>Top Page</h1>
+    <li v-for="room in roomList" :key="room.uuid">
+      <p>{{ room.title }}</p>
+    </li>
   </v-container>
 </template>
 
 <script>
   export default {
     name: 'TopPage',
-    data: () => ({}),
+    data: () => ({
+      roomList: []
+    }),
     methods: {
       getRooms() {
         const axios = require('axios');
 
         axios.get('http://localhost:8888/rooms')
-          .then(function (response) {
-            console.log(response);
-          })
+          .then(this.updateRoomList)
           .catch(function (error) {
             console.log(error);
           });
+      },
+      updateRoomList(response) {
+        this.roomList = response.data;
       }
     },
     created: function() {
@@ -26,3 +32,9 @@
     }
   }
 </script>
+
+<style scoped>
+li {
+  list-style: none;
+}
+</style>
