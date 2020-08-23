eb5e12eb0eecf599628d454b66bbefaaf3a846e0
enokawa <enokawa00@gmail.com>
2020-08-23 17:36:44 +0900

    call get room API

--- frontend/src/pages/TopPage.vue
+++ frontend/src/pages/TopPage.vue
@@ -1,27 +1,28 @@
 <template>
   <v-container>
     <h1>Top Page</h1>
-    <v-row>
-      <v-col cols="12">
-        <li v-for="message in messages" :key="message.title">
-          <v-card
-            outlined
-          >
-            <v-list-item three-line style="text-align: left;">
-              <v-list-item-content>
-                <v-list-item-title >{{ message.title }}</v-list-item-title>
-                <v-list-item-subtitle>{{ message.value }}</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </v-card>
-        </li>
-      </v-col>
-    </v-row>
   </v-container>
 </template>
 
 <script>
   export default {
-    name: 'TopPage'
+    name: 'TopPage',
+    data: () => ({}),
+    methods: {
+      getRooms() {
+        const axios = require('axios');
+
+        axios.get('http://localhost:8888/rooms')
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+    },
+    created: function() {
+      this.getRooms();
+    }
   }
 </script>
